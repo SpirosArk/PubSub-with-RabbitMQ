f6e70fe644a2ee8194291ba3fe44eb5986f72dbc
Spiros Arkoudelis <s.arkoudelis@cloudf.in>
2023-11-23 00:34:43 +0200

    Pre Docker Implementation

--- FormulaAirline.API/Program.cs
+++ FormulaAirline.API/Program.cs
@@ -1,9 +1,14 @@
+using FormulaAirline.API.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<IMessageProducer, MessageProducer>();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
---
+++ FormulaAirline.API/Services/IMessageProducer.cs
@@ -0,0 +1,8 @@
+﻿namespace FormulaAirline.API.Services
+{
+    public interface IMessageProducer
+    {
+        public void SendingMessage<T> (T message);
+
+    }
+}
